0e1e7a8d9eed5e27d676812f78d133e287ea846f
skulb <skulb@FoolMoon>
2024-12-23 22:58:15 +0700

    template pos 0.1

--- src/PointOfSalesApps.java
+++ src/PointOfSalesApps.java
@@ -8,22 +8,109 @@ public class PointOfSalesApps extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        welcText = new javax.swing.JLabel();
+        adminName = new javax.swing.JLabel();
+        welcText1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        welcText2 = new javax.swing.JLabel();
+        welcText3 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(850, 600));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 465, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 287, Short.MAX_VALUE)
-        );
+        welcText.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/images.png"))); // NOI18N
+        welcText.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        welcText.setMaximumSize(new java.awt.Dimension(28, 28));
+        welcText.setMinimumSize(new java.awt.Dimension(28, 28));
+        welcText.setName(""); // NOI18N
+        getContentPane().add(welcText, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 0, 30, 30));
+
+        adminName.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        adminName.setText("Admin");
+        getContentPane().add(adminName, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 13, -1, -1));
+
+        welcText1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        welcText1.setForeground(new java.awt.Color(255, 0, 51));
+        welcText1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/logout.png"))); // NOI18N
+        welcText1.setName(""); // NOI18N
+        getContentPane().add(welcText1, new org.netbeans.lib.awtextra.AbsoluteConstraints(920, 5, -1, -1));
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/userMng.png"))); // NOI18N
+        jButton1.setText("Kelola Pengguna");
+        jButton1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton1.setMargin(new java.awt.Insets(3, 1, 3, 3));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(594, 2, 130, 30));
+
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/POS.png"))); // NOI18N
+        jButton2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton2.setLabel("Point of Sales");
+        jButton2.setMargin(new java.awt.Insets(3, 1, 3, 3));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(153, 2, 130, 30));
+
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/tabelBarang.png"))); // NOI18N
+        jButton3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton3.setLabel("Point of Sales");
+        jButton3.setMargin(new java.awt.Insets(3, 1, 3, 3));
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 2, 130, 30));
+
+        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/tabelTransaksi.png"))); // NOI18N
+        jButton4.setText("Tabel Transaksi");
+        jButton4.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton4.setMargin(new java.awt.Insets(3, 1, 3, 3));
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(447, 2, 130, 30));
+
+        welcText2.setText("Selamat datang,");
+        welcText2.setName(""); // NOI18N
+        getContentPane().add(welcText2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 0, -1, -1));
+
+        welcText3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        welcText3.setForeground(new java.awt.Color(204, 0, 51));
+        welcText3.setText("Logout");
+        welcText3.setName(""); // NOI18N
+        getContentPane().add(welcText3, new org.netbeans.lib.awtextra.AbsoluteConstraints(870, 5, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     public static void main(String args[]) {
  
@@ -39,5 +126,14 @@ public class PointOfSalesApps extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel adminName;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel welcText;
+    private javax.swing.JLabel welcText1;
+    private javax.swing.JLabel welcText2;
+    private javax.swing.JLabel welcText3;
     // End of variables declaration//GEN-END:variables
 }
