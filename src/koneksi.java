e87738eaf0d1752b3c334169007ccf3d25afb749
Kelio Xenelly <kelioxenelly0507@gmail.com>
2024-12-26 11:21:34 +0700

    Finalisasi Backend KelolaPengguna

--- src/koneksi.java
+++ src/koneksi.java
@@ -5,20 +5,21 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class koneksi {
-    private String url="jdbc:mysql://localhost:3306/pboposdb?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private String url = "jdbc:mysql://localhost:3306/pboposdb";
     private String user = "root";
     private String password = "";
     private Connection conn;
-    
+
     public void connect() {
         try {
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Koneksi berhasil");
         } catch (SQLException ex) {
-            Logger.getLogger(koneksi.class.getName()).log(Level.SEVERE, null, ex);
+            conn = null; // Ensure conn is null on failure
+            Logger.getLogger(koneksi.class.getName()).log(Level.SEVERE, "Database connection failed!", ex);
         }
     }
-    
+
     public Connection getConnection() {
         return conn;
     }
